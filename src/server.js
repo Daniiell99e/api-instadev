65c30e593bde41a84ec900ae46feb23e33f68a81
Daniiell99e <danielsilva99e@gmail.com>
2025-09-06 15:29:29 -0300

    first commit

---
+++ src/server.js
@@ -0,0 +1,12 @@
+require('dotenv').config();
+const routes = require('./routes');
+const express = require('express');
+const app = express();
+
+app.use(express.json());
+app.use(routes);
+
+const { PORT } = process.env;
+app.listen(PORT, () => {
+    console.log('Server running on port 3000');
+});
